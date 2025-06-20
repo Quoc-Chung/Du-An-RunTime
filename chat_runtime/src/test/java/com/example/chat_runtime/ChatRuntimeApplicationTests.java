4f1a4e7227fd01ec14a6223a29ab9a4bb9e81c55
Tu Quoc Chung <123059617+Quoc-Chung@users.noreply.github.com>
2025-06-20 18:39:14 +0700

    initial project

---
+++ chat_runtime/src/test/java/com/example/chat_runtime/ChatRuntimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat_runtime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRuntimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
